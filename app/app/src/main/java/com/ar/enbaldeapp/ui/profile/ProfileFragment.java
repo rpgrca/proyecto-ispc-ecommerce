1118f627c4b7992c177341c7acbc6d984496cdf7
Roberto <rpgrca@gmail.com>
2024-05-02 22:54:34 -0300

    add profile to navigation bar

---
+++ app/app/src/main/java/com/ar/enbaldeapp/ui/profile/ProfileFragment.java
@@ -0,0 +1,41 @@
+package com.ar.enbaldeapp.ui.profile;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.ar.enbaldeapp.R;
+import com.ar.enbaldeapp.databinding.FragmentDashboardBinding;
+import com.ar.enbaldeapp.databinding.FragmentProfileBinding;
+import com.ar.enbaldeapp.ui.dashboard.DashboardViewModel;
+
+public class ProfileFragment extends Fragment {
+
+    private FragmentProfileBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        ProfileViewModel profileViewModel =
+                new ViewModelProvider(this).get(ProfileViewModel.class);
+
+        binding = FragmentProfileBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textProfile;
+        profileViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
