55d8a2448c762fdb3b30587b8479df49a8f74976
Roberto <rpgrca@gmail.com>
2024-05-04 01:19:49 -0300

    hide add button in product detail when not logged in

--- app/app/src/main/java/com/ar/enbaldeapp/ui/details/ProductDetailActivity.java
+++ app/app/src/main/java/com/ar/enbaldeapp/ui/details/ProductDetailActivity.java
@@ -1,18 +1,24 @@
 package com.ar.enbaldeapp.ui.details;
 
+import static com.ar.enbaldeapp.ui.IntentConstants.CURRENT_USER_FOR_DETAIL;
 import static com.ar.enbaldeapp.ui.IntentConstants.PRODUCT_FOR_DETAIL;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.ar.enbaldeapp.R;
 import com.ar.enbaldeapp.models.Product;
+import com.ar.enbaldeapp.models.User;
 
 public class ProductDetailActivity extends AppCompatActivity {
     private Product product;
+    private User currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +27,17 @@ public class ProductDetailActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         product = (Product)intent.getSerializableExtra(PRODUCT_FOR_DETAIL);
+        currentUser = (User)intent.getSerializableExtra(CURRENT_USER_FOR_DETAIL);
 
         ((TextView)this.findViewById(R.id.textViewDetailName)).setText(product.getName());
         ((TextView)this.findViewById(R.id.textViewDetailDescription)).setText(product.getDescription());
-        ((TextView)this.findViewById(R.id.editNumberDetailAmount)).setText("1");
+
+        if (currentUser == null) {
+            EditText editText = this.findViewById(R.id.editNumberDetailAmount);
+            editText.setVisibility(View.GONE);
+
+            Button button = this.findViewById(R.id.buttonDetailAdd);
+            button.setVisibility(View.GONE);
+        }
     }
 }
