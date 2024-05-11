c79c00f4889e29cfc15bae921d58b28337f889cd
Roberto <rpgrca@gmail.com>
2024-05-11 18:40:29 -0300

    get instagram, facebook, whatsapp from configuration

--- front-end/enbalde/src/app/shared/footer/footer.component.ts
+++ front-end/enbalde/src/app/shared/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component, ElementRef } from '@angular/core';
+import { Configuracion } from 'src/app/models/modelo.configuracion';
+import { ConfiguracionesService } from 'src/app/services/configuraciones.service';
 
 @Component({
   selector: 'app-footer',
@@ -8,8 +10,29 @@ import { Component, ElementRef } from '@angular/core';
 export class FooterComponent {
 
   itemEnbalde = '../assets/img/3-svg.png'
+  instagram: string = '';
+  instagramUrl: string = ''
+  facebook: string = '';
+  facebookUrl: string = '';
+  whatsapp: string = '';
 
-  constructor(private elementRef: ElementRef) {}
+  constructor(private elementRef: ElementRef, private configuracionesService: ConfiguracionesService) {}
+
+
+  ngOnInit(): void {
+    this.configuracionesService.obtenerConfiguraciones()
+      .subscribe((configuraciones: Configuracion[]) => {
+        let c: { [id: string] : string } = {};
+        configuraciones.forEach((el, index) => c[el.nombre] = el.valor);
+
+        this.instagram = c['instagram'];
+        this.instagramUrl = `https://www.instagram.com/${this.instagram}`;
+        this.facebook = c['facebook'];
+        this.facebookUrl = `https://www.facebook.com/${this.facebook}`;
+        this.whatsapp = c['whatsapp'];
+        this.itemEnbalde = c['logo'];
+      });
+  }
 
 
   volverArriba() {
