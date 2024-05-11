67901e32025a98f6bafd53f5753a92c5b84f6321
Roberto <rpgrca@gmail.com>
2024-05-11 20:03:01 -0300

    add configuration screen

--- front-end/enbalde/src/app/pages/configuraciones/configuraciones.component.ts
+++ front-end/enbalde/src/app/pages/configuraciones/configuraciones.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { constantes } from 'src/environment/constantes';
+import { ConfiguracionesService } from 'src/app/services/configuraciones.service';
+import { Configuracion } from 'src/app/models/modelo.configuracion';
 
 @Component({
   selector: 'app-configuraciones',
-  standalone: true,
-  imports: [],
   templateUrl: './configuraciones.component.html',
-  styleUrl: './configuraciones.component.css'
+  styleUrls: ['./configuraciones.component.css'],
+  providers: [ ConfiguracionesService ]
 })
-export class ConfiguracionesComponent {
 
+export class ConfiguracionesComponent {
+  readonly constantes = constantes;
+  crearConfiguracionForm!: FormGroup;
+
+  @Input() configuraciones: Configuracion[];
+
+  constructor(private formBuilder: FormBuilder, public configuracionesService : ConfiguracionesService) {
+    this.configuraciones = [];
+  }
+
+  ngOnInit(): void {
+    this.configuracionesService.obtenerConfiguraciones().subscribe((configuraciones: Configuracion[]) => this.configuraciones = configuraciones);
+    this.crearConfiguracionForm = this.formBuilder.group({
+      nombre: ["", [Validators.required, Validators.minLength(constantes.MINIMO_NOMBRE_CONFIGURACION), Validators.maxLength(constantes.MAXIMO_NOMBRE_CONFIGURACION)]],
+      valor: ["", [Validators.required, Validators.minLength(constantes.MINIMO_VALOR_CONFIGURACION), Validators.maxLength(constantes.MAXIMO_VALOR_CONFIGURACION)]]
+    });
+  }
+
+  get nombre() { return this.crearConfiguracionForm.get('nombre'); }
+  get valor() { return this.crearConfiguracionForm.get('valor'); }
+
+  crear(value: any) {
+    this.configuracionesService.crear(value.nombre, value.valor)
+      .subscribe((configuracion: Configuracion) => {
+        this.refrescar();
+        this.crearConfiguracionForm.reset();
+      })
+  }
+
+  refrescar(): void {
+    this.configuracionesService.obtenerConfiguraciones()
+      .subscribe((configuraciones: Configuracion[]) => this.configuraciones = configuraciones);
+  }
 }
---
+++ front-end/enbalde/src/app/pages/item-configuracion/item-configuracion.component.ts
@@ -0,0 +1,61 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { constantes } from 'src/environment/constantes';
+import { Configuracion } from 'src/app/models/modelo.configuracion';
+import { ConfiguracionesService } from 'src/app/services/configuraciones.service';
+
+@Component({
+  selector: 'app-item-configuracion',
+  templateUrl: './item-configuracion.component.html',
+  styleUrls: ['./item-configuracion.component.css'],
+  providers: [ConfiguracionesService]
+})
+
+export class ItemConfiguracionComponent {
+  editarItemConfiguracionForm!: FormGroup;
+  editando?: Configuracion
+
+  @Input() configuracion?: Configuracion;
+  @Input() odd: boolean;
+  @Output() refrescar: EventEmitter<any> = new EventEmitter<any>();
+
+  constructor(private formBuilder: FormBuilder, private configuracionesService: ConfiguracionesService) {
+    this.editando = undefined;
+    this.configuracion = undefined;
+    this.odd = false;
+  }
+
+  ngOnInit(): void {
+    this.editarItemConfiguracionForm = this.formBuilder.group({
+      nuevoNombre: ["", [Validators.required, Validators.minLength(constantes.MINIMO_NOMBRE_CONFIGURACION), Validators.maxLength(constantes.MAXIMO_NOMBRE_CONFIGURACION)]],
+      nuevoValor: ["", [Validators.required, Validators.minLength(constantes.MINIMO_VALOR_CONFIGURACION), Validators.maxLength(constantes.MAXIMO_VALOR_CONFIGURACION)]]
+    })
+  }
+
+  get nuevoNombre() { return this.editarItemConfiguracionForm.get('nuevoNombre'); }
+
+  get nuevoValor() { return this.editarItemConfiguracionForm.get('nuevoValor'); }
+
+  editar(configuracion: Configuracion) {
+    this.editarItemConfiguracionForm.get("nuevoNombre")?.setValue(configuracion.nombre);
+    this.editarItemConfiguracionForm.get("nuevoValor")?.setValue(configuracion.valor);
+    this.editando = configuracion;
+  }
+
+  borrar(configuracion: Configuracion) {
+    this.configuracionesService.borrar(configuracion)
+      .subscribe((_: any) => this.refrescar.emit());
+  }
+
+  grabar(configuracion: Configuracion, value: any) {
+    this.configuracionesService.modificar(configuracion, value.nuevoNombre, value.nuevoValor)
+      .subscribe((nuevaConfiguracion: Configuracion) => {
+        this.editando = undefined;
+        this.refrescar.emit();
+    });
+  }
+
+  cancelar(configuracion: Configuracion) {
+    this.editando = undefined;
+  }
+}
