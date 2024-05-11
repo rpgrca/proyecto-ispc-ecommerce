7b01a9fb0a257b033ff59bac9a235a1051384471
Roberto <rpgrca@gmail.com>
2024-05-11 19:33:39 -0300

    add configuration component

---
+++ front-end/enbalde/src/app/pages/configuraciones/configuraciones.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-configuraciones',
+  standalone: true,
+  imports: [],
+  templateUrl: './configuraciones.component.html',
+  styleUrl: './configuraciones.component.css'
+})
+export class ConfiguracionesComponent {
+
+}
