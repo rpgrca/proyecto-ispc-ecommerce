f0fd6525b3d904527fa95690bc133faa02ad6e21
Roberto <rpgrca@gmail.com>
2024-05-18 17:54:36 -0300

    add tests for api request

---
+++ app/app/src/test/java/com/ar/enbaldeapp/services/ApiRequestMust.java
@@ -0,0 +1,27 @@
+package com.ar.enbaldeapp.services;
+
+import static com.ar.enbaldeapp.support.Constants.USERNAME;
+
+import static org.junit.Assert.assertEquals;
+
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
+import org.junit.Test;
+
+public class ApiRequestMust {
+    @Test
+    public void beBuiltCorrectly() {
+        ApiRequest sut = new ApiRequest.Builder()
+                .addContentDisposition("usuario", USERNAME)
+                .addContentDisposition("clave", 12345678)
+                .addBoundary("separator")
+                .Build();
+
+        MatcherAssert.assertThat(sut.getData(), CoreMatchers.containsString("Content-Disposition: form-data; name=\"usuario\""));
+        MatcherAssert.assertThat(sut.getData(), CoreMatchers.containsString(USERNAME));
+        MatcherAssert.assertThat(sut.getData(), CoreMatchers.containsString("Content-Disposition: form-data; name=\"clave\""));
+        MatcherAssert.assertThat(sut.getData(), CoreMatchers.containsString("12345678"));
+        MatcherAssert.assertThat(sut.getData(), CoreMatchers.containsString("separator"));
+        assertEquals("separator", sut.getBoundary());
+    }
+}
