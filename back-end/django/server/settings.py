288bd248f15ddb6fac67d3f5daf9fab33e6d02bc
LeonardoEC <LeonardoDanielAV@proton.me>
2024-05-22 19:32:22 -0300

    Pantallas de product_details y Fragment_catalogue

--- back-end/django/server/settings.py
+++ back-end/django/server/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'enbalde',
-        'USER': 'root',
+        'USER': '',
         'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306',
