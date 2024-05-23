4956de7dce3afea07fddc9991c77b4ab852c8a52
Roberto <rpgrca@gmail.com>
2024-05-23 10:23:58 -0300

    convert to fragment

--- app/app/src/main/java/com/ar/enbaldeapp/ui/recover/RecoverPasswordFragment.java
+++ app/app/src/main/java/com/ar/enbaldeapp/ui/recover/RecoverPasswordFragment.java
@@ -71,7 +71,7 @@ public class RecoverPasswordFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_recover_password, container, false);
+        View view = inflater.inflate(R.layout.fragment_recover_password_old, container, false);
 
         TextView textView = view.findViewById(R.id.recoverAlreadyAccountTextView);
         textView.setOnClickListener(v -> {
